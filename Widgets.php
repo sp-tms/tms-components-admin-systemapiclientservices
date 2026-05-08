6f3d51eebc53e3ff84d396c5d845a85201903d54
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-08 23:49:45 +0530

    Initial Commit

---
+++ Widgets.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Apps\Tms\Components\System\Api\Client\Services;
+
+use System\Base\Providers\ModulesServiceProvider\Modules\Components\ComponentsWidgets;
+
+class Widgets extends ComponentsWidgets
+{}
